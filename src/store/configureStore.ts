a05516ad1f3774e58673083257ceba042c5a7d0b
MSTEAM\lelchugin <leo.solo@mail.ru>
2018-03-14 12:19:52 +0700

    + Глобальные интерфейсы и отслеживаение состояния

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -4,12 +4,22 @@ import { History } from 'history'
 import { reducers, StoreState } from '../reducers'
 
 export function configureStore(history: History): Store<StoreState> {
+    const { __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: devToolsCompose } = window
+    const composeEnhancers = devToolsCompose || compose
     const store = createStore<StoreState>(
-        reducers,
-        applyMiddleware(
-            routerMiddleware(history)
-        )
-    )
+		reducers,
+		composeEnhancers(
+			applyMiddleware(
+				routerMiddleware(history)
+			)
+		)
+	)
 
-    return store
+    if (module.hot) {
+        module.hot.accept(() => {
+			store.replaceReducer(require('../reducers').reducers)
+		})
+	}
+
+	return store
 }
---
+++ src/types/global.d.ts
@@ -0,0 +1,9 @@
+interface Window {
+	__REDUX_DEVTOOLS_EXTENSION_COMPOSE__: Function
+ }
+
+interface NodeModule {
+	hot: {
+		accept: Function
+	}
+}
